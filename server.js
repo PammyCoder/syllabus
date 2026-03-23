bcbd62a6fcfe40e2616274d00de44c7e75b37800
Pammy Kumari <pammy2018@ismpatna.ac.in>
2026-03-23 15:34:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18032,8 +18032,7 @@ app.get("/thankyou", (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+
+
 });
